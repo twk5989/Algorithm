d229fe59f5639f1f24e987a0a63b1bfb4b390139
twk5989 <a37225989@gmail.com>
2025-07-28 16:40:29 +0900

    이론 최종

--- 6주차/이론/1-5.py
+++ 6주차/이론/1-5.py
@@ -7,4 +7,15 @@ def card_conv(x:int, r:int) -> str:
     n = len(str(x))       #변환하기 전의 자릿수
     
     print(f'{r:2} | {x:{n}d}')
-    
+    while x > 0:
+        print('  +' + (n + 2) * '-')
+        
+        if x // r:
+            print(f'{r:2} | {x // r:{n}d}.....{x % r}')
+        else:
+            print(f'        {x // r:{n}d}.....{x % r}')
+        d += dchar [x % r]
+        x //= r
+        
+    return d[::-1] # 역순으로
+        
