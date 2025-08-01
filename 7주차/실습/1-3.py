051f28e80961729d24e435b6df767316c2f92f0d
twk5989 <a37225989@gmail.com>
2025-08-01 23:54:26 +0900

    7주차 최종

--- 7주차/실습/1-3.py
+++ 7주차/실습/1-3.py
@@ -3,13 +3,15 @@
 
 from typing import Sequence, Any
 
-def analyze(data: Sequence[Any]) -> None: #함수 정의해서 data(정보의 이름)로 정보가 함수에 입력
-    # None= 이 함수가 어떠한걸 반환하지는 않는다.그냥 출력값만 표시한다라는 의미임
+def analyze(data: Sequence[Any]) -> None: #반환하지 않는다 -> return을 하지않고 print 값만 출력한다.
     
     print("최댓값:", max(data))
     print("원소 개수:", len(data))
+    
 
+
+
+analyze((10, 20, 30))
 
 analyze([5, 2, 9, 1])
 
-analyze((10, 20, 30))
