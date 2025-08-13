adfe7448aa5fbff59e692a8d198f6c2d2891473f
twk5989 <a37225989@gmail.com>
2025-08-13 20:35:11 +0900

    9주차 끝

---
+++ 9주차/실습/1-4.py
@@ -0,0 +1,33 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/388351
+
+def solution(schedules, timelogs, startday):
+    
+    def m(t):   #일단 분으로 만드는 함수를 설정함
+        return (t // 100) * 60 + (t % 100)
+
+    n = len(schedules)  #n은 사원수임
+    
+    time = []           #설정값 +10을 해줄 time
+    
+    for s in schedules:
+        time.append(m(s) + 10)
+
+    answer = 0 
+    
+    for i in range(len(schedules)): #n을 적어도 상관없지
+        
+        for d in range(7):
+            
+            dow = (startday - 1 + d) % 7 + 1
+            
+            if dow in (6, 7):
+                continue
+            
+            if m(timelogs[i][d]) > time[i]:
+                # 지각이면 바로 다음 직원으로 넘어감
+                break
+            
+        else:
+            answer += 1
+
+    return answer
