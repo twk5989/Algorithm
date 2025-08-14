452729d712ea0965c7b08c5d4c20391c3da8e908
twk5989 <a37225989@gmail.com>
2025-08-14 18:02:01 +0900

    ㅈ됫다

--- 10주차/이론/1-1.py
+++ 10주차/이론/1-1.py
@@ -8,6 +8,7 @@ class Node:
     """해시를 구성하는 노드"""
 
     def __init__(self, key: Any, value: Any, next: Node) -> None:
+        
         """초기화"""
         self.key   = key    # 키
         self.value = value  # 값
