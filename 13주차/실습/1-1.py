a5da2669bda1e51d52956add2d70356c7b852954
twk5989 <a37225989@gmail.com>
2025-09-17 16:53:37 +0900

    RE: 한 문제 남음

--- 13주차/실습/1-1.py
+++ 13주차/실습/1-1.py
@@ -34,3 +34,35 @@ for _ in range(Q):
             break  # 찾았으면 더 이상 비교 안 함
 
     print(C)
+
+
+#리팩토링 버전
+#접근 방식: 그 전에는 순차적으로 비교하며 찾아서 비교 횟수를 세어서 구현을 했었는데 그게 아닌 배열에서 내가 검색하고자하는 단어를 일단 찾고 처음-찾은 단어까지 잘라서 배열을 새롭게 만들자 
+
+def TW(a, b):
+    i = 0
+    while i < len(a) and i < len(b) and a[i] == b[i]:
+        i += 1
+    return i + 1 
+
+n = int(input())
+
+for _ in range(n):
+    word = input()
+    db.append(word)
+
+
+Q = int(input())
+
+for _ in range(Q):
+    search = input()
+    total = 0
+    
+    #찾을 위치를 미리 계산
+    #근데 여기서 오해를 할 수 있음 5개 다 들어가는거 아니냐. 근데 위에 TW의 조건을 확인해 보시길
+    index = db.index(search)
+    
+    for word in db[:index + 1]:
+        total += TW(search, word)
+        
+    print(total)
--- 13주차/실습/1-2.py
+++ 13주차/실습/1-2.py
@@ -1,9 +1,9 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/388351
+#https://school.programmers.co.kr/learn/courses/30/lessons/388351
 
 def solution(schedules, timelogs, startday):
     
     def m(t):   #일단 분으로 만드는 함수를 설정함
-        return (t // 100) * 60 + (t % 100)
+        return (t // 100) * 60 + (t % 100)  #950은 9시 50분이기에
 
     n = len(schedules)  #n은 사원수임
     
@@ -35,3 +35,41 @@ def solution(schedules, timelogs, startday):
             answer += 1
 
     return answer
+
+
+
+
+#리팩토링 코드
+def solution(schedules, timelogs, startday):
+    
+    answer = 0
+    
+    st = startday
+    
+    for x, y in enumerate(schedules): #x는 사람 번호 인덱스. y는 그 사람의 예정 출근시간
+        
+        tw = True  #이 사람이 출근에 성공여부.일단 True로 설정
+        
+        if y%100 >= 50: #이거를 왜 적었을까. 왜 내가 50을 더했을까 애들아?
+            
+            y += 50
+            
+        else:
+            y += 10
+            
+        for i in timelogs[x]: #x번째 사람의 출근 기록 리스트와 i는 그 날의 실제 출근 시간
+            
+            if not i <= y and not 6 <= startday <= 7:
+                tw = False
+                break
+            startday += 1 #그 날 검사 했으니. 그 다음날로 넘어감
+            
+            if startday == 8: #일주일은 7일이니 8일로 되면 1로 돌아감
+                startday = 1
+                
+        if tw == True:
+            answer += 1
+        startday = st #다음사람 검사해야하니 초기화
+        
+    return answer
+
